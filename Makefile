1c299e6845bfbf91a4731406604b6649d1a42ab8
AgentGosdepa <kot60068@gmail.com>
2022-08-19 17:11:10 +0700

    add FreeRTOS

--- Makefile
+++ Makefile
@@ -1,23 +1,30 @@
+INC += -I.
+INC += -I../FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM3
+INC += -I../FreeRTOS/FreeRTOS/Source/include
+BUILD_DIR = ./build
 
-AS = arm-none-eabi-as
+All:
+	arm-none-eabi-as -mcpu=cortex-m3 -c startup/startup_stm32f103xb.s -o $(BUILD_DIR)/startup_stm32f103xb.o
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c main.c -o $(BUILD_DIR)/main.o
+	
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/croutine.c -o $(BUILD_DIR)/croutine.o
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/event_groups.c -o $(BUILD_DIR)/event_groups.o 
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_1.c -o $(BUILD_DIR)/heap_1.o 
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_2.c -o $(BUILD_DIR)/heap_2.o 
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_3.c -o $(BUILD_DIR)/heap_3.o 
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_4.c -o $(BUILD_DIR)/heap_4.o 
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/portable/MemMang/heap_5.c -o $(BUILD_DIR)/heap_5.o 
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/list.c -o $(BUILD_DIR)/list.o
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/queue.c -o $(BUILD_DIR)/queue.o 
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/stream_buffer.c -o $(BUILD_DIR)/stream_buffer.o 
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/tasks.c -o $(BUILD_DIR)/tasks.o 
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/timers.c -o $(BUILD_DIR)/timers.o 
+	arm-none-eabi-gcc -mcpu=cortex-m3 -O0 $(INC) -c /home/qwe/FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c -o $(BUILD_DIR)/port.o
 
-#yt-dlp1 -i -f 'bestvideo+bestaudio' --merge-output-format mkv -o "%(playlist)s/%(playlist_index)s %(title)s [%(id)s].%(ext)s" -a 2.txt
+	arm-none-eabi-ld -T ld/STM32F103XB_FLASH.ld -o $(BUILD_DIR)/proj_name.elf $(BUILD_DIR)/main.o $(BUILD_DIR)/startup_stm32f103xb.o $(BUILD_DIR)/croutine.o $(BUILD_DIR)/event_groups.o $(BUILD_DIR)/heap_1.o $(BUILD_DIR)/list.o $(BUILD_DIR)/queue.o $(BUILD_DIR)/stream_buffer.o $(BUILD_DIR)/tasks.o  $(BUILD_DIR)/timers.o  $(BUILD_DIR)/port.o
+	arm-none-eabi-objcopy -O binary $(BUILD_DIR)/proj_name.elf $(BUILD_DIR)/proj_name.bin
+	openocd -f board/st_nucleo_f103rb.cfg -f interface/stlink.cfg -c "init; reset halt; flash write_image erase $(BUILD_DIR)/proj_name.bin 0x08000000; verify_image $(BUILD_DIR)/proj_name.bin; reset; exit"
 
-#reset halt
-#flash write_image erase fin.elf
-#verify_image fin.elf
-
-all:
-	arm-none-eabi-as -mcpu=cortex-m3 -c stup/startup_stm32f103xb.s -o bin/startup_stm32f103xb.o
-	arm-none-eabi-gcc -mcpu=cortex-m3 -c -O0 main.c -o bin/main.o
-	arm-none-eabi-ld -T ld/STM32F103XB_FLASH.ld -o bin/fin.elf bin/startup_stm32f103xb.o bin/main.o
-	arm-none-eabi-objcopy -O binary bin/fin.elf bin/fin.bin
-ocd:
-	openocd -f target/nrf52.cfg -f interface/stlink.cfg
-
-flash:
-	openocd -f board/st_nucleo_f103rb.cfg -f interface/stlink.cfg -c \
-	"init; reset halt; flash write_image erase bin/fin.elf; verify_image bin/fin.elf; reset; exit"
-
+.PHONY: clean
 clean:
-	rm bin/*
+	rm $(BUILD_DIR)/*
--- main.c
+++ main.c
@@ -1,74 +1,87 @@
-void Reserved_IRQHandler(void)
+#define F_CPU 8000000UL
+//#include "stm32f10x.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+ 
+// Отдадочная затычка. Сюда можно вписать код обработки ошибок.
+#define ERROR_ACTION(CODE,POS)    do{}while(0)
+ 
+ void *memcpy(void *dest, const void *src, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        ((char*)dest)[i] = ((char*)src)[i];
+    }
+    return dest;
+}
+
+void* memset(void* buf, char z, size_t bytes)
+{
+    if (buf)
+    {
+       char* tmp_mem = (char*)buf;
+       while (bytes--) *tmp_mem++ = z;
+    }
+    
+    return buf;
+}
+
+// Задача моргалка. Просто так. Мигает диодиком на порту LED3
+void vBlinker (void *pvParameters)
 {
  while(1)
  {
-
+  *((unsigned*)0x4001080CUL) ^= (0x1UL << 5U);
+  vTaskDelay(100);      // Выдержка 600мс
+  *((unsigned*)0x4001080CUL) ^= (0x1UL << 5U);
+  vTaskDelay(100);       // Выдержка 20мс
  }
 }
-
-void NMI_Handler(void)
+ 
+ 
+int main(void)
 {
-  while(1)
-  {
-    /* nothing to be run here */
-  }
-}
+  *((unsigned*)0x40021018UL) |= (0x1UL << 2U);
+  *((unsigned*)0x40010800UL) &= ~(0x3UL << 22U);
+  *((unsigned*)0x40010800UL) |= (0x1UL << 20U);
 
-void HardFault_Handler(void)
-{
-  while(1)
-  {
-    /* nothing to be run here */
-  }
-}
-
-void SVC_Handler(void)
-{
-  while(1)
-  {
-    /* nothing to be run here */
-  }
-}
-
-void PendSV_Handler(void)
-{
-  while(1)
-  {
-    /* nothing to be run here */
-  }
-}
-
-void SysTick_Handler(void)
-{
-  while(1)
-  {
-    /* nothing to be run here */
-  }
+ 
+if(pdTRUE != xTaskCreate(vBlinker,"Blinker",  configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL)) ERROR_ACTION(TASK_NOT_CREATE,0); 
+// Создаем задачи. Если при создании задачи возвращенный параметр не TRUE, то обрабатываем ошибку.
+// vBlinker       - это имя функции которая будет задачей. 
+// "Blinker"      - текстовая строка для отладки. Может быть любой, но длина ограничина в конфиге ОС. Ну и память она тоже ест немного.
+// configMINIMAL_STACK_SIZE   - размер стека для задачи. Определяется опытным путем. В данном случае стоит системный минимум ,т.к. 
+//        Задачи используют очень мало памяти. Должно хватить. 
+// NULL       - передаваемый pvParameters в задачу. В данном случае не нужен, потому NULL. Но можно задаче при создании передевать
+//        Разные данные, для инициализации или чтобы различать две копии задачи между собой. 
+// tskIDLE_PRIORITY + 1   - приоритет задачи. В данный момент выбран минимально возможный. На 1 пункт выше чем IDLE
+// NULL       - тут вместо NULL можно было вписать адрес переменной типа xTaskHandle в которую бы упал дескриптор задачи. 
+//        Например так: xTaskCreate(vBlinker,"Blinker",configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &xBlinkerTask); 
+//        где xBlinkerTask это глобальная переменная обьявленная как xTaskHandle xBlinkerTask; глобальная - чтобы ее везде
+//        было видно. Отовсюду она была доступна. Но можно и как static обьявить или еще каким образом передать хэндл.
+//        И зная эту переменную и то что там дескриптор этой задачи мы могли бы из другой задачи ее грохнуть, поменять
+//        приоритет или засуспендить. В общем, управлять задачей. Но в моем примере это не требуется. 
+//        Остальные аналогично.
+ 
+// Запускаем диспетчер и понеслась.   
+vTaskStartScheduler();
 }
 
 
-#include "stm32f103xb.h"
-#define PA 0x40010800UL
 
 
-
-
-int main()
+int main22()
 {
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+  *((unsigned*)0x40021018UL) |= (0x1UL << 2U);
+  *((unsigned*)0x40010800UL) &= ~(0x3UL << 22U);
+  *((unsigned*)0x40010800UL) |= (0x1UL << 20U);
 
-    GPIOA->CRL &= ~GPIO_CRL_CNF5;
-    GPIOA->CRL |= GPIO_CRL_MODE5_0;
-  //*((unsigned*)PA) = (1<<(4*5));
+  while(1){
+    for(volatile int i=0; i<200000; i++){}
+    *((unsigned*)0x4001080CUL) ^= (0x1UL << 5U);
+  }
 
-
-    while(1){
-      for(volatile int i=0; i<200000; i++){}
-      GPIOA->ODR ^= GPIO_ODR_ODR5;
-    }
-
-
-    while(1){}
-
-    return 0;
+  while(1){}
+  return 0;
 }
