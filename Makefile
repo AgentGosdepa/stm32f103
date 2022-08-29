147ca7d5bc106480cfe7f07771a86c2806301ad3
AgentGosdepa <kot60068@gmail.com>
2022-08-29 12:32:20 +0700

    подключение nrf driver

--- Makefile
+++ Makefile
@@ -2,6 +2,14 @@ INC += -I.
 #INC += -I../FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM3
 INC += -I../FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM4F
 INC += -I../FreeRTOS/FreeRTOS/Source/include
+
+INC += -I/home/qwe/1_Projects/work_nrf_10_2021/sdk_nRF5_SDK_17.0.0_9d13099/modules/nrfx
+INC += -I/home/qwe/1_Projects/work_nrf_10_2021/sdk_nRF5_SDK_17.0.0_9d13099/modules/nrfx/hal
+INC += -I/home/qwe/1_Projects/work_nrf_10_2021/sdk_nRF5_SDK_17.0.0_9d13099/modules/nrfx/mdk
+INC += -I/home/qwe/1_Projects/work_nrf_10_2021/sdk_nRF5_SDK_17.0.0_9d13099/modules/nrfx/templates
+INC += -I/home/qwe/1_Projects/work_nrf_10_2021/sdk_nRF5_SDK_17.0.0_9d13099/modules/nrfx/drivers/include
+INC += -I/home/qwe/1_Projects/work_nrf_10_2021/sdk_nRF5_SDK_17.0.0_9d13099/modules/nrfx/drivers/src/prs
+INC += -I/home/qwe/1_Projects/work_nrf_10_2021/sdk_nRF5_SDK_17.0.0_9d13099/components/toolchain/cmsis/include
 BUILD_DIR = ./build
 
 #LDFILE = /home/qwe/1_Projects/work_nrf_10_2021/sdk_nRF5_SDK_17.0.0_9d13099/config/nrf52840/armgcc/generic_gcc_nrf52.ld
@@ -10,7 +18,7 @@ LDFILE  = ld/nrf52840_xxaa.ld
 STARTUPFILE = startup/gcc_startup_nrf52840.S
 #STARTUPFILE = startup/startup_stm32f103xb.s
 LDFLAGS = --gc-sections
-FLAGS = -mcpu=cortex-m4 -O0 -mfloat-abi=hard -march=armv7e-m+fp
+FLAGS = -mcpu=cortex-m4 -Os -mfloat-abi=hard -march=armv7e-m+fp -DNRF52840_XXAA
 
 All:
 	arm-none-eabi-as -mcpu=cortex-m4 -c $(STARTUPFILE) -o $(BUILD_DIR)/gcc_startup_nrf52840.o
@@ -32,13 +40,17 @@ All:
 	
 	#arm-none-eabi-gcc $(FLAGS) $(INC) -c ../FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c -o $(BUILD_DIR)/port.o
 	arm-none-eabi-gcc $(FLAGS) $(INC) -c ../FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c -o $(BUILD_DIR)/port.o
+	
+	arm-none-eabi-gcc $(FLAGS) $(INC) -c /home/qwe/1_Projects/work_nrf_10_2021/sdk_nRF5_SDK_17.0.0_9d13099/modules/nrfx/drivers/src/nrfx_uart.c -o $(BUILD_DIR)/nrfx_uart.o
+	arm-none-eabi-gcc $(FLAGS) $(INC) -c /home/qwe/1_Projects/work_nrf_10_2021/sdk_nRF5_SDK_17.0.0_9d13099/modules/nrfx/drivers/src/prs/nrfx_prs.c -o $(BUILD_DIR)/nrfx_prs.o
 
 	#arm-none-eabi-ld -T $(LDFILE) -o $(BUILD_DIR)/proj_name.elf $(BUILD_DIR)/main.o $(BUILD_DIR)/startup_stm32f103xb.o $(BUILD_DIR)/croutine.o $(BUILD_DIR)/event_groups.o $(BUILD_DIR)/heap_1.o $(BUILD_DIR)/list.o $(BUILD_DIR)/queue.o $(BUILD_DIR)/tasks.o  $(BUILD_DIR)/timers.o  $(BUILD_DIR)/port.o
 		
-	arm-none-eabi-ld -T $(LDFILE) $(LDFLAGS) -o $(BUILD_DIR)/proj_name.elf $(BUILD_DIR)/main.o $(BUILD_DIR)/gcc_startup_nrf52840.o $(BUILD_DIR)/croutine.o $(BUILD_DIR)/event_groups.o $(BUILD_DIR)/heap_1.o $(BUILD_DIR)/list.o $(BUILD_DIR)/queue.o $(BUILD_DIR)/tasks.o $(BUILD_DIR)/timers.o $(BUILD_DIR)/port.o
+	arm-none-eabi-ld -T $(LDFILE) $(LDFLAGS) -o $(BUILD_DIR)/proj_name.elf $(BUILD_DIR)/main.o $(BUILD_DIR)/gcc_startup_nrf52840.o $(BUILD_DIR)/croutine.o $(BUILD_DIR)/event_groups.o $(BUILD_DIR)/heap_1.o $(BUILD_DIR)/list.o $(BUILD_DIR)/queue.o $(BUILD_DIR)/tasks.o $(BUILD_DIR)/timers.o $(BUILD_DIR)/port.o $(BUILD_DIR)/nrfx_uart.o $(BUILD_DIR)/nrfx_prs.o
 	
 	arm-none-eabi-objcopy -O binary $(BUILD_DIR)/proj_name.elf $(BUILD_DIR)/proj_name.bin
-	
+
+nrf:
 	openocd -f board/nordic_nrf52_dk.cfg -c "init; reset halt; nrf5 mass_erase; program build/proj_name.elf verify; reset; exit" 
 	#openocd -f board/st_nucleo_f103rb.cfg -f interface/stlink.cfg -c "init; reset halt; flash write_image erase $(BUILD_DIR)/proj_name.bin 0x08000000; verify_image $(BUILD_DIR)/proj_name.bin; reset; exit"
 
--- main.c
+++ main.c
@@ -3,10 +3,19 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
+#include "nrf_gpio.h"
+#include "nrfx_uart.h"
+
+#define LED_1          NRF_GPIO_PIN_MAP(0,13)
+#define LED_2          NRF_GPIO_PIN_MAP(0,14)
+#define LED_3          NRF_GPIO_PIN_MAP(0,15)
+#define LED_4          NRF_GPIO_PIN_MAP(0,16)
  
 // Отдадочная затычка. Сюда можно вписать код обработки ошибок.
 #define ERROR_ACTION(CODE,POS)    do{}while(0)
- 
+#define APP_ERROR_CHECK(d) (void)(d)
+
+
  void *memcpy(void *dest, const void *src, size_t n)
 {
     for (size_t i = 0; i < n; i++)
@@ -27,14 +36,63 @@ void* memset(void* buf, char z, size_t bytes)
     return buf;
 }
 
+void uart0_handler(nrfx_uart_event_t const *p_event, void *p_context)
+{
+
+}
+
+#define UART_RTS NRF_GPIO_PIN_MAP(0, 5)
+#define UART_TX NRF_GPIO_PIN_MAP(0, 6)
+#define UART_CTS NRF_GPIO_PIN_MAP(0, 7)
+#define UART_RX NRF_GPIO_PIN_MAP(0, 8)
+
+static nrfx_uart_t uart0 = NRFX_UART_INSTANCE(0);
+
+
+
+void uart_init(void)
+{
+    /*
+    //true init
+    nrf_gpio_pin_set(UART_TX);
+    nrf_gpio_cfg_output(UART_TX);
+
+    nrf_gpio_pin_set(UART_RTS);
+    nrf_gpio_cfg_output(UART_RTS);
+
+    nrf_gpio_cfg_input(UART_RX, NRF_GPIO_PIN_NOPULL);
+    nrf_gpio_cfg_input(UART_CTS, NRF_GPIO_PIN_NOPULL);
+
+    NRF_UART0->BAUDRATE = NRFX_UART_DEFAULT_CONFIG_BAUDRATE;
+    NRF_UART0->CONFIG = UART_CONFIG_PARITY_Excluded | UART_CONFIG_HWFC_Enabled;
+    NRF_UART0->PSEL.RTS = UART_RTS;
+    NRF_UART0->PSEL.TXD = UART_TX;
+    NRF_UART0->PSEL.CTS = UART_CTS;
+    NRF_UART0->PSEL.RXD = UART_RX;
+
+    NRF_UART0->ENABLE = UART_ENABLE_ENABLE_Enabled;
+    */
+
+    nrfx_uart_config_t uart_cfg = NRFX_UART_DEFAULT_CONFIG;
+    uart_cfg.pseltxd = UART_TX;
+    uart_cfg.pselrxd = UART_RX;
+    uart_cfg.pselcts = UART_CTS;
+    uart_cfg.pselrts = UART_RTS;    
+
+    uint32_t err_code = nrfx_uart_init(&uart0, &uart_cfg, uart0_handler);
+    if (err_code != 0x0BAD0000) nrf_gpio_pin_clear(LED_4);
+}
+
 // Задача моргалка. Просто так. Мигает диодиком на порту LED3
 void vBlinker1(void *pvParameters)
 {
     while(1)
-    {
-    *((unsigned*)0x50000504UL) ^= (0x1UL << 13U | 0x1UL << 14U);
+    {    
+    nrf_gpio_pin_toggle(LED_3);
+    //*((unsigned*)0x50000504UL) ^= (0x1UL << 13U | 0x1UL << 14U);
     vTaskDelay(100);      // Выдержка 600мс
-    *((unsigned*)0x50000504UL) ^= (0x1UL << 13U | 0x1UL << 14U);
+    nrf_gpio_pin_toggle(LED_4);
+    //*((unsigned*)0x50000504UL) ^= (0x1UL << 13U | 0x1UL << 14U);
     vTaskDelay(100);       // Выдержка 20мс
     }
 }
@@ -43,77 +101,66 @@ void vBlinker2(void *pvParameters)
 {
     while(1)
     {
-    vTaskDelay(100); 
-    *((unsigned*)0x50000504UL) ^= (0x1UL << 15U | 0x1UL << 16U);
+    vTaskDelay(100);
+    nrf_gpio_pin_toggle(LED_1);
+    //*((unsigned*)0x50000504UL) ^= (0x1UL << 15U | 0x1UL << 16U);
     vTaskDelay(100);      // Выдержка 600мс
-    *((unsigned*)0x50000504UL) ^= (0x1UL << 15U | 0x1UL << 16U);
+    nrf_gpio_pin_toggle(LED_2);
+    //*((unsigned*)0x50000504UL) ^= (0x1UL << 15U | 0x1UL << 16U);
     //vTaskDelay(100);       // Выдержка 20мс
     }
+}
+
+void vUart0()
+{
+    while(1)
+    {
+        vTaskDelay(5000);
+        nrf_gpio_pin_toggle(LED_1);
+
+        NRF_UART0->TXD = 'a';
+        NRF_UART0->TASKS_STARTTX = UART_TASKS_STARTTX_TASKS_STARTTX_Trigger;
+        vTaskDelay(100);
+
+        //NRF_UART0->TASKS_STARTRX = UART_TASKS_STARTRX_TASKS_STARTRX_Trigger;
+        //char b = NRF_UART0->RXD;
+        /*if (b == '2')
+            nrf_gpio_pin_toggle(LED_2);
+        else if (b == '3')
+            nrf_gpio_pin_toggle(LED_3);*/
+
+        //uint32_t err_code = NRFX_SUCCESS;
+        //taskENTER_CRITICAL();
+        //char *asd = "asd\n\0";
+        //uint32_t err_code = nrfx_uart_tx(&uart0, asd, 4);
+        //taskEXIT_CRITICAL();
+        //if (err_code != NRFX_SUCCESS) nrf_gpio_pin_clear(LED_4);
+    }
+             
+}
+
+void led_init()
+{
+    nrf_gpio_cfg_output(LED_1);
+    nrf_gpio_cfg_output(LED_2);
+    nrf_gpio_cfg_output(LED_3);
+    nrf_gpio_cfg_output(LED_4);
+    nrf_gpio_pin_set(LED_1);
+    nrf_gpio_pin_set(LED_2);
+    nrf_gpio_pin_set(LED_3);
+    nrf_gpio_pin_set(LED_4);
 }
  
 int main(void)
 {
-    *((unsigned*)0x50000734UL) = (1UL<<0UL | 1UL<<1UL | 0UL<<2UL | 0UL<<8UL | 0UL<<16UL); //out p013
-    *((unsigned*)0x50000738UL) = (1UL<<0UL | 1UL<<1UL | 0UL<<2UL | 0UL<<8UL | 0UL<<16UL); //out p014
-    *((unsigned*)0x5000073CUL) = (1UL<<0UL | 1UL<<1UL | 0UL<<2UL | 0UL<<8UL | 0UL<<16UL); //out p015
-    *((unsigned*)0x50000740UL) = (1UL<<0UL | 1UL<<1UL | 0UL<<2UL | 0UL<<8UL | 0UL<<16UL); //out p016
-    *((unsigned*)0x50000508UL) = (0x1UL << 13U | 0x1UL << 14U | 0x1UL << 15U | 0x1UL << 16U); //dis led
-
+    led_init();
+    uart_init();
  
     //if(pdTRUE != xTaskCreate(vBlinker1, "Blinker1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL)) ERROR_ACTION(TASK_NOT_CREATE,0);
     
-    if(pdTRUE != xTaskCreate(vBlinker2, "Blinker2", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL)) ERROR_ACTION(TASK_NOT_CREATE,0); 
-// Создаем задачи. Если при создании задачи возвращенный параметр не TRUE, то обрабатываем ошибку.
-// vBlinker       - это имя функции которая будет задачей. 
-// "Blinker"      - текстовая строка для отладки. Может быть любой, но длина ограничина в конфиге ОС. Ну и память она тоже ест немного.
-// configMINIMAL_STACK_SIZE   - размер стека для задачи. Определяется опытным путем. В данном случае стоит системный минимум ,т.к. 
-//        Задачи используют очень мало памяти. Должно хватить. 
-// NULL       - передаваемый pvParameters в задачу. В данном случае не нужен, потому NULL. Но можно задаче при создании передевать
-//        Разные данные, для инициализации или чтобы различать две копии задачи между собой. 
-// tskIDLE_PRIORITY + 1   - приоритет задачи. В данный момент выбран минимально возможный. На 1 пункт выше чем IDLE
-// NULL       - тут вместо NULL можно было вписать адрес переменной типа xTaskHandle в которую бы упал дескриптор задачи. 
-//        Например так: xTaskCreate(vBlinker,"Blinker",configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, &xBlinkerTask); 
-//        где xBlinkerTask это глобальная переменная обьявленная как xTaskHandle xBlinkerTask; глобальная - чтобы ее везде
-//        было видно. Отовсюду она была доступна. Но можно и как static обьявить или еще каким образом передать хэндл.
-//        И зная эту переменную и то что там дескриптор этой задачи мы могли бы из другой задачи ее грохнуть, поменять
-//        приоритет или засуспендить. В общем, управлять задачей. Но в моем примере это не требуется. 
-//        Остальные аналогично.
- 
-// Запускаем диспетчер и понеслась.   
+    //if(pdTRUE != xTaskCreate(vBlinker2, "Blinker2", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL)) ERROR_ACTION(TASK_NOT_CREATE,0); 
+
+    if(pdTRUE != xTaskCreate(vUart0, "uart0", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL)) ERROR_ACTION(TASK_NOT_CREATE,0); 
+
     vTaskStartScheduler();
-}
-
-
-
-
-int main22()
-{
-  *((unsigned*)0x40021018UL) |= (0x1UL << 2U);
-  *((unsigned*)0x40010800UL) &= ~(0x3UL << 22U);
-  *((unsigned*)0x40010800UL) |= (0x1UL << 20U);
-
-  while(1){
-    for(volatile int i=0; i<200000; i++){}
-    *((unsigned*)0x4001080CUL) ^= (0x1UL << 5U);
-  }
-
-  while(1){}
-  return 0;
-}
-
-int main33()
-{
-    *((unsigned*)0x50000734UL) = (1UL<<0UL | 1UL<<1UL | 0UL<<2UL | 0UL<<8UL | 0UL<<16UL); //out p013
-    *((unsigned*)0x50000738UL) = (1UL<<0UL | 1UL<<1UL | 0UL<<2UL | 0UL<<8UL | 0UL<<16UL); //out p014
-    *((unsigned*)0x5000073CUL) = (1UL<<0UL | 1UL<<1UL | 0UL<<2UL | 0UL<<8UL | 0UL<<16UL); //out p015
-    *((unsigned*)0x50000740UL) = (1UL<<0UL | 1UL<<1UL | 0UL<<2UL | 0UL<<8UL | 0UL<<16UL); //out p016
-    *((unsigned*)0x50000508UL) = (0x1UL << 13U | 0x1UL << 14U | 0x1UL << 15U | 0x1UL << 16U); //dis led
-    
-    while(1){
-            for(volatile int i=0; i<2000000; i++){}
-              *((unsigned*)0x50000504UL) ^= (0x1UL << 15U | 0x1UL << 16U);
-    }
-
-    while(1){}
-    return 0;
 }
